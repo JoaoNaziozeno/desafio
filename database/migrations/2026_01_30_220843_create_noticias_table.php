2bae3cc0f93ace1aef5f76d3769e2beae6d18e1b
João Naziozeno <joaonazi@gmail.com>
2026-01-30 20:54:30 -0300

    Redefinição do nome das tabelas pela migration e do arquivo da Models

--- database/migrations/2026_01_30_220843_create_noticias_table.php
+++ database/migrations/2026_01_30_220843_create_noticias_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('notícias', function (Blueprint $table) {
+        Schema::create('noticias', function (Blueprint $table) {
             $table->id();
 
             $table->foreignId('users_id')
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('notícias');
+        Schema::dropIfExists('noticias');
     }
 };
